7e810a7d9ba6d78963353d14616c3b287adc27f2
dietrich <dietrich@newskylabs.net>
2019-10-14 17:57:05 +0200

    Added __main__.py files

---
+++ newskylabs/tools/bookblock/__main__.py
@@ -0,0 +1,27 @@
+"""newskylabs/tools/bookblock/__main__.py:
+
+Main entry point for the `nsl-bookblock` tool.
+
+"""
+
+__author__      = "Dietrich Bollmann"
+__email__       = "dietrich@formgames.org"
+__copyright__   = "Copyright 2019 Dietrich Bollmann"
+__license__     = "Apache License 2.0, http://www.apache.org/licenses/LICENSE-2.0"
+__date__        = "2019/10/11"
+
+__version__     = '0.0.1.dev1'
+
+from newskylabs.tools.bookblock.scripts.bookblock import bookblock
+
+## =========================================================
+## Main
+## ---------------------------------------------------------
+ 
+if __name__ == '__main__':
+    bookblock()
+
+## =========================================================
+## =========================================================
+
+## fin.
